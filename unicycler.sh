df3f5ef35e81f812161c9071b18c9af74d04bd2c
Nahuel PPilquinao <nawel.pilquinao@gmail.com>
2024-04-01 07:23:41 +0200

    AMRfinder search success!

---
+++ unicycler.sh
@@ -0,0 +1,76 @@
+unicycler\
+    -1 VA1046_2020/S63_R1.fastq.gz\
+    -2 VA1046_2020/S63_R2.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA1046_2020\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA1101_2021/1101_21_S87_R1_001.fastq.gz\
+    -2 VA1101_2021/1101_21_S87_R2_001.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA1101_2021\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA1184_2021/1184_21_S88_R1_001.fastq.gz\
+    -2 VA1184_2021/1184_21_S88_R2_001.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA1184_2021\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA1565_2021/1565_21_S75_R1_001.fastq.gz\
+    -2 VA1565_2021/1565_21_S75_R2_001.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA1565_2021\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA1788_2021/S78_R1.fastq.gz\
+    -2 VA1788_2021/S78_R2.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA1788_2021\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA2067_2020/2067_20_S67_R1_001.fastq.gz\
+    -2 VA2067_2020/2067_20_S67_R2_001.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA2067_2020\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA2464_2020/VA2464_20_R1_trim.fastq.gz\
+    -2 VA2464_2020/VA2464_20_R2_trim.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA2464_2020\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA2588_2020/S69_R1.fastq.gz\
+    -2 VA2588_2020/S69_R2.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA2588_2020\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA418_2022/S72_R1.fastq.gz\
+    -2 VA418_2022/S72_R2.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA418_2022\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA585_2022/S85_R1.fastq.gz\
+    -2 VA585_2022/S85_R2.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA585_2022\
+    --threads 32\
+    --spades_options "--threads 100"
+
+unicycler\
+    -1 VA692_2022/S80_R1.fastq.gz\
+    -2 VA692_2022/692_22_S80_R2_001.fastq.gz\
+    --out ~/group_storage/npilquinao/ISP/2carbapenemase/assembly/VA692_2022\
+    --threads 32\
+    --spades_options "--threads 100"
